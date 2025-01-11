e16cc9af475a2df39793940564fc3ad1223634bd
Garma <dedlayn8@gmail.com>
2025-01-11 20:55:07 +0700

    test

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,3 +1,48 @@
-int main(void){
-    while(1){}
+#include "../Inc/init.h"
+
+uint8_t flag1 = 0;
+uint8_t flag2 = 0;
+
+int main(void)
+{
+
+    GPIO_Ini();
+    while (1)
+    {
+        if ((READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_6) != 0))
+        {
+            flag1 = 1;
+            flag2 = 0;
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
+            for (int i = 0; i < 500000; i++)
+                ;
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+        }
+        if (READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_7) != 0)
+        {
+            flag1 = 0;
+            flag2 = 1;
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
+            for (int i = 0; i < 500000; i++)
+                ;
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+        }
+        if (flag1 == 1)
+        {
+            SET_BIT(GPIOC->MODER, GPIO_MODER_MODE12_0);
+            SET_BIT(GPIOC->BSRR, GPIO_BSRR_BS12);
+        }
+        if (flag2 == 1)
+        {
+            SET_BIT(GPIOC->BSRR, GPIO_BSRR_BR12);
+            SET_BIT(GPIOC->MODER, GPIO_MODER_MODE12);
+            if (READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_12) != 0)
+            {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
+                for (int i = 0; i < 500000; i++)
+                    ;
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+            }
+        }
+    }
 }
--- build/init.d
+++ build/init.d
@@ -1,4 +1,4 @@
-Build/main.o: Core/Src/main.c Core/Src/../Inc/init.h \
+Build/init.o: Core/Src/init.c Core/Inc/init.h \
  C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/stm32f429xx.h \
  CMSIS/Devices/../include/core_cm4.h \
  CMSIS/Devices/../include/cmsis_version.h \
@@ -6,7 +6,7 @@ Build/main.o: Core/Src/main.c Core/Src/../Inc/init.h \
  CMSIS/Devices/../include/cmsis_gcc.h \
  CMSIS/Devices/../include/mpu_armv7.h \
  C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/system_stm32f4xx.h
-Core/Src/../Inc/init.h:
+Core/Inc/init.h:
 C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/stm32f429xx.h:
 CMSIS/Devices/../include/core_cm4.h:
 CMSIS/Devices/../include/cmsis_version.h:
