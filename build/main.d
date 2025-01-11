6ce6f82a24e679f9aa6f7ee9286b45aaadf93b98
Garma <dedlayn8@gmail.com>
2025-01-11 21:07:10 +0700

    TEST

--- build/main.d
+++ build/main.d
@@ -1,12 +1,15 @@
 Build/main.o: Core/Src/main.c Core/Src/../Inc/init.h \
+ C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/stm32f4xx.h \
  C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/stm32f429xx.h \
  CMSIS/Devices/../include/core_cm4.h \
  CMSIS/Devices/../include/cmsis_version.h \
  CMSIS/Devices/../include/cmsis_compiler.h \
  CMSIS/Devices/../include/cmsis_gcc.h \
  CMSIS/Devices/../include/mpu_armv7.h \
- C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/system_stm32f4xx.h
+ C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/system_stm32f4xx.h \
+ C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/stm32f429xx.h
 Core/Src/../Inc/init.h:
+C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/stm32f4xx.h:
 C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/stm32f429xx.h:
 CMSIS/Devices/../include/core_cm4.h:
 CMSIS/Devices/../include/cmsis_version.h:
@@ -14,3 +17,4 @@ CMSIS/Devices/../include/cmsis_compiler.h:
 CMSIS/Devices/../include/cmsis_gcc.h:
 CMSIS/Devices/../include/mpu_armv7.h:
 C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/system_stm32f4xx.h:
+C:/Users/dedla/Documents/Labstm/Laboratory_Practice/CMSIS/Devices/Inc/stm32f429xx.h:
